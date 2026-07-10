0e434fcd4c2cabafbddcdecd7c3b1ea736f0f185
Sohag Abir <sohagabir112@gmail.com>
2026-07-10 11:57:04 +0600

    Time: 9 ms (52.75%), Space: 11.8 MB (65.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,7 @@ public:
         int n = s.size();
         
         unordered_map<char, int>f;
-        int res = INT_MIN;
+        int res = 0;
         for(high=0; high<n; high++){
             f[s[high]]++;
             while(f[s[high]]>1){
@@ -15,6 +15,6 @@ public:
             }
             res = max(res, high-low+1);
         }
-        return res==INT_MIN? 0:res;
+        return res;
     }
 };
